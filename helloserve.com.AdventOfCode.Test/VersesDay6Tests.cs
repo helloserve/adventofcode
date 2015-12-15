40352c92976f146dcab45bc1b24b31de2a17c61e
Henk Roux <henk@globalkinetic.com>
2015-12-15 12:34:20 +0200

    split unit tests into a file per day

---
+++ helloserve.com.AdventOfCode.Test/VersesDay6Tests.cs
@@ -0,0 +1,47 @@
+﻿using helloserve.com.AdventOfCode.Test.Base;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloserve.com.AdventOfCode.Test
+{
+    [TestClass]
+    public class VersesDay6Tests : VersesTests
+    {
+        [TestMethod]
+        public void TestDay6_Part1_Example1()
+        {
+            string input = "turn on 0,0 through 999,999";
+            int result = Verses.Day6_Part1(input);
+            Assert.IsTrue(result == 1000000);
+        }
+
+        [TestMethod]
+        public void TestDay6_Part1_Example2()
+        {
+            string input = "toggle 0,0 through 999,0";
+            int result = Verses.Day6_Part1(input);
+            Assert.IsTrue(result == 1000);
+        }
+
+        [TestMethod]
+        public void TestDay6_Part1()
+        {
+            string input = FromFile("day6.txt");
+            int result = Verses.Day6_Part1(input);
+            Assert.IsTrue(result == 569999);
+        }
+
+        [TestMethod]
+        public void TestDay6_Part2()
+        {
+            string input = FromFile("day6.txt");
+            int result = Verses.Day6_Part2(input);
+            Assert.IsTrue(result == 17836115);
+        }
+
+    }
+}
--- helloserve.com.AdventOfCode.Test/VersesDay8Tests.cs
+++ helloserve.com.AdventOfCode.Test/VersesDay8Tests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using helloserve.com.AdventOfCode.Test.Base;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
