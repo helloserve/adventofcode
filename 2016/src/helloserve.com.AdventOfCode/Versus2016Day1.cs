1493e16f466788a5c0df66e1e5bfd949023088c2
Henk Roux <henk@globalkinetic.com>
2016-12-01 10:14:25 +0200

    2016 Day 1 Part 1

---
+++ 2016/src/helloserve.com.AdventOfCode/Versus2016Day1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace helloserve.com.AdventOfCode
+{
+    public static class Day1Extensions
+    {
+        public static char Turn(this char currentDirection, string turn)
+        {
+            string directions = "NESW";
+            int index = directions.IndexOf(currentDirection);
+
+            if (turn == "L")
+                index++;
+
+            if (turn == "R")
+                index--;
+
+            if (index > 3)
+                index = index - 4;
+            if (index < 0)
+                index = 4 + index;
+
+            return directions[index];
+        }
+
+        public static Vector2 GetDirectionVector(this char direction)
+        {
+            switch (direction)
+            {
+                case 'N':
+                    return Vector2.North;
+                case 'E':
+                    return Vector2.East;
+                case 'S':
+                    return Vector2.South;
+                case 'W':
+                    return Vector2.West;
+                default:
+                    throw new ArgumentException();
+            }
+        }
+
+        public static Vector2 Move(this Vector2 position, char direction, int steps)
+        {
+            return direction.GetDirectionVector()
+                .Multiply(steps)
+                .Add(position);
+        }
+    }
+
+    public class Vector2
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Vector2 Multiply(int scalar)
+        {
+            X *= scalar;
+            Y *= scalar;
+            return this;
+        }
+
+        public Vector2 Add(Vector2 vector)
+        {
+            X += vector.X;
+            Y += vector.Y;
+            return this;
+        }
+
+        public int RectilinearDistance()
+        {
+            return Math.Abs(X) + Math.Abs(Y);
+        }
+
+        public static Vector2 Zero
+        {
+            get { return new Vector2() { X = 0, Y = 0 }; }
+        }
+
+        public static Vector2 North
+        {
+            get { return new Vector2() { X = 1, Y = 0 }; }
+        }
+
+        public static Vector2 East
+        {
+            get { return new Vector2() { X = 0, Y = -1 }; }
+        }
+
+        public static Vector2 South
+        {
+            get { return new Vector2() { X = -1, Y = 0 }; }
+        }
+
+        public static Vector2 West
+        {
+            get { return new Vector2() { X = 0, Y = 1 }; }
+        }
+    }
+
+    public class Versus2016Day1
+    {
+        public Versus2016Day1() { }
+
+        public int Part1(string input)
+        {
+            Vector2 position = Vector2.Zero;
+
+            char direction = 'N';
+
+            string[] commands = input.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < commands.Length; i++)
+            {
+                string command = commands[i].Trim();
+                string dir = command.Substring(0, 1);
+                int steps = int.Parse(command.Substring(1, command.Length - 1));
+
+                direction = direction.Turn(dir);
+                
+
+                position = position.Move(direction, steps);
+            }
+
+            return position.RectilinearDistance();
+        }
+    }
+}
--- 2016/test/helloserve.com.AdventOfCode.Tests/Versus2016Day1.cs
+++ 2016/test/helloserve.com.AdventOfCode.Tests/Versus2016Day1.cs
@@ -1,14 +1,71 @@
-﻿using System;
+﻿using helloserve.com.AdventOfCode.Tests.Base;
+using System;
 using Xunit;
 
 namespace helloserve.com.AdventOfCode.Tests
 {
-    public class Versus2016Day1
+    public class Versus2016Day1 : TestClass
     {
+        AdventOfCode.Versus2016Day1 verses = new AdventOfCode.Versus2016Day1();
+        
         [Fact]
-        public void Test1() 
+        public void Part1_L0Zero() 
         {
-            Assert.True(true);
+            Assert.True(verses.Part1("L0") == 0);
+        }
+
+        [Fact]
+        public void Part1_L1_1()
+        {
+            Assert.True(verses.Part1("L1") == 1);
+        }
+
+        [Fact]
+        public void Part1_L1R1_2()
+        {
+            Assert.True(verses.Part1("L1, R1") == 2);
+        }
+
+        [Fact]
+        public void Part1_L5L0L5_Zero()
+        {
+            Assert.True(verses.Part1("L5, L0, L5") == 0);
+        }
+
+        [Fact]
+        public void Part1_R5R0R5_Zero()
+        {
+            Assert.True(verses.Part1("R5, R0, R5") == 0);
+        }
+        
+        [Fact]
+        public void Part1_StepsNW()
+        {
+            Assert.True(verses.Part1("L1, R1, L1, R1, L1") == 5);
+        }
+
+        [Fact]
+        public void Part1_Ex1()
+        {
+            Assert.True(verses.Part1("R2, L3") == 5);
+        }
+
+        [Fact]
+        public void Part1_Ex2()
+        {
+            Assert.True(verses.Part1("R2, R2, R2") == 2);
+        }
+
+        [Fact]
+        public void Part1_Ex3()
+        {
+            Assert.True(verses.Part1("R5, L5, R5, R3") == 12);
+        }
+
+        [Fact]
+        public void Part1_Part1()
+        {
+            Assert.True(verses.Part1(ReadTextSource("1A.txt")) == 278);
         }
     }
 }
