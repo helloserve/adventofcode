0cd8aa51511ed486768162b07ce8a363732b69e0
helloserve <helloserve@gmail.com>
2016-12-04 15:36:54 +0200

    day 4 part 1

---
+++ 2016/src/helloserve.com.AdventOfCode/Verses2016Day04.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace helloserve.com.AdventOfCode
+{
+    public class Room
+    {
+        public int SectorId { get; set; }
+
+        private string _encryptedValue { get; set; }
+        private List<CharacterOrder> _encryptedCharOrders { get; set; }
+        private string _checksum { get; set; }
+
+        public Room(string roomCode)
+        {
+            Regex regex = new Regex(@"(?<encrypted>[a-z-]*)(?<sectorId>\d*)\[{1}(?<checksum>[a-z]{5})\]{1}$");
+            Match match = regex.Match(roomCode);
+            if (!match.Success)
+                return;
+
+            SectorId = int.Parse(match.Groups["sectorId"].Value);
+            _encryptedValue = match.Groups["encrypted"].Value.Replace("-", "");
+            _checksum = match.Groups["checksum"].Value;
+            Decrypt();
+        }
+
+        public bool IsValid
+        {
+            get
+            {
+                bool result = true;
+                for (int i = 0; i < _checksum.Length; i++)
+                {
+                    result &= _checksum[i] == _encryptedCharOrders[i].Character;
+                }
+
+                return result;
+            }
+        }
+
+        private void Decrypt()
+        {
+            _encryptedCharOrders = new List<CharacterOrder>();
+
+            Dictionary<char, CharacterOrder> orderLookup = new Dictionary<char, AdventOfCode.Room.CharacterOrder>();
+
+            for (int i = 0; i < _encryptedValue.Length; i++)
+            {
+                char c = _encryptedValue[i];
+                if (orderLookup.ContainsKey(c))
+                    orderLookup[c].Order++;
+                else
+                    orderLookup.Add(c, new CharacterOrder() { Character = c, Order = 1 });
+            }
+
+            _encryptedCharOrders = orderLookup.Values.ToList();
+            _encryptedCharOrders.Sort();
+            _encryptedCharOrders = _encryptedCharOrders.Take(_checksum.Length).ToList();
+        }
+
+        private class CharacterOrder : IComparable
+        {
+            public char Character { get; set; }
+            public int Order { get; set; }
+
+            public int CompareTo(object obj)
+            {
+                CharacterOrder other = obj as CharacterOrder;
+                if (other == null)
+                    throw new ArgumentException();
+
+                int result = other.Order.CompareTo(Order);
+                if (result == 0)
+                    result = Character.CompareTo(other.Character);
+
+                return result;
+            }
+        }
+    }
+
+    public class Verses2016Day04
+    {
+        public int Part1(string input)
+        {
+            string[] lines = input.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            List<Room> rooms = new List<AdventOfCode.Room>();
+            foreach (var line in lines)
+            {
+                rooms.Add(new AdventOfCode.Room(line));
+            }
+
+            return rooms.Sum(r => r.IsValid ? r.SectorId : 0);
+        }
+    }
+}
---
+++ 2016/test/helloserve.com.AdventOfCode.Tests/Verses2016Day04Tests.cs
@@ -0,0 +1,40 @@
+﻿using helloserve.com.AdventOfCode.Tests.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace helloserve.com.AdventOfCode.Tests
+{
+    public class Verses2016Day04Tests : TestClass
+    {
+        Verses2016Day04 verses = new Verses2016Day04();
+
+        [Fact]
+        public void Part1_Simple_IsRoom_1()
+        {
+            Assert.True(verses.Part1("abcde-1[abcde]") == 1);
+            Assert.True(verses.Part1("xyabc-1[abcxy]") == 1);
+            Assert.True(verses.Part1("aa-bb-cc-xy-1[abcxy]") == 1);
+        }
+
+        [Fact]
+        public void Part1_Ex()
+        {
+            Assert.True(verses.Part1("aaaaa-bbb-z-y-x-123[abxyz]") == 123);
+            Assert.True(verses.Part1("a-b-c-d-e-f-g-h-987[abcde]") == 987);            
+            Assert.True(verses.Part1("not-a-real-room-404[oarel]") == 404);
+            Assert.True(verses.Part1("totally -real-room-200[decoy]") == 0);
+
+            Assert.True(verses.Part1("aaaaa-bbb-z-y-x-123[abxyz]\r\na-b-c-d-e-f-g-h-987[abcde]\r\nnot-a-real-room-404[oarel]\r\ntotally -real-room-200[decoy]") == 1514);
+        }
+
+        [Fact]
+        public void Part1_Part1()
+        {
+            Assert.True(verses.Part1(ReadTextSource("4.txt")) == 361724);
+        }
+    }
+}
+
