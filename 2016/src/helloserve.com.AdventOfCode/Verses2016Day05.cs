9983fbf0f745ff85194c9d55ab1c1cd37e74ea85
helloserve <helloserve@gmail.com>
2016-12-05 08:28:11 +0200

    day 5 part 1 WIP

---
+++ 2016/src/helloserve.com.AdventOfCode/Verses2016Day05.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloserve.com.AdventOfCode
+{
+    public class Verses2016Day05
+    {
+        public string Part1(string input)
+        {
+            List<Task> tasks = new List<Task>();
+            string code = string.Empty;
+            object codeObject = new object();
+            for (int i = 0; i < Environment.ProcessorCount; i++)
+            {
+                int startIndex = i;
+                tasks.Add(Task.Run(() => GetCodeItem(input, startIndex, Environment.ProcessorCount))
+                    .ContinueWith(a =>
+                    {
+                        lock (codeObject)
+                        {
+                            code = $"{code}{a.Result}";
+                        }
+                    }));
+            }
+            while (true)
+            {
+                if (tasks.Count == 0)
+                    break;
+
+                if (tasks[0].IsCompleted)
+                    tasks.RemoveAt(0);
+            }
+
+            return code;
+        }
+
+        public string GetCodeItem(string input, int startIndex, int skip, int? range = int.MaxValue)
+        {
+            for (int i = startIndex; i < range; i += skip)
+            {
+                MD5 md5 = MD5.Create();
+                md5.Initialize();
+                string hash = Encoding.ASCII.GetString(md5.ComputeHash(Encoding.ASCII.GetBytes($"{input}{i}")));
+                if (hash.StartsWith("00000"))
+                    return hash.Substring(5, 1);
+            }
+
+            return null;
+        }
+
+        //public string GetHexString(byte[] buffer)
+        //{
+        //    string hexString = string.Empty;
+        //    for (int i = 0; i < buffer.Length; i++)
+        //    {
+        //        hexString = $"{hexString}{buffer[i]:x}";
+        //    }
+
+        //    return hexString;
+        //}
+    }
+}
---
+++ 2016/test/helloserve.com.AdventOfCode.Tests/Verses2016Day05Tests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace helloserve.com.AdventOfCode.Tests
+{
+    public class Verses2016Day05Tests
+    {
+        private Verses2016Day05 verses = new Verses2016Day05();
+
+        [Fact]
+        public void Par1t1_Ex1()
+        {            
+            //Assert.True(verses.GetCodeItem("abc", 3231929, 1) == "1");
+            Assert.True(verses.GetCodeItem("abc", 5017308, 1) == "8");
+
+            //Assert.True(verses.Part1("abc") == "1");
+        }
+    }
+}
