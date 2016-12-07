91600509798f4a391025848f864f07a64f93a0a2
helloserve <helloserve@gmail.com>
2016-12-07 08:32:05 +0200

    day 7 part 1 WIP

---
+++ 2016/src/helloserve.com.AdventOfCode/Verses2016Day07.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace helloserve.com.AdventOfCode
+{
+    public class Verses2016Day07
+    {
+        public string[] ParseIp(string ip)
+        {
+            Regex regex = new Regex(@"(\D*)\[(\D*)\](\D*)");
+            Match match = regex.Match(ip);
+            if (match.Success)
+                return new string[] { match.Groups[1].Value, match.Groups[2].Value, match.Groups[3].Value };
+            else
+                return new string[3];
+        }
+
+        public bool ContainsAbba(string part)
+        {
+            int i = 0;
+            while (i + 3 < part.Length)
+            {
+                if (part[i] == part[i + 3] && part[i + 1] == part[i + 2] && part[i] != part[i + 1])
+                {
+                    return true;
+                }
+
+                i++;
+            }
+
+            return false;
+        }
+
+        public bool IsTls(string ip)
+        {
+            string[] ipParts = ParseIp(ip);
+
+            if (ipParts[0].Length < 4)
+                return false;
+
+            bool abbaInHypernet = ContainsAbba(ipParts[1]);
+            bool result = ContainsAbba(ipParts[0]) && !abbaInHypernet;
+
+            if (!result && ipParts.Length > 2)
+            {
+                if (ipParts[2].Length < 4)
+                    return false;
+
+                result = ContainsAbba(ipParts[2]) && !abbaInHypernet;
+            }
+
+            return result;
+        }
+
+        public int Part1(string input)
+        {
+            List<string> ips = input.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+            return ips.Select(x => IsTls(x) ? 1 : 0).Sum();
+        }
+    }
+}
---
+++ 2016/test/helloserve.com.AdventOfCode.Tests/Verses2016Day07Tests.cs
@@ -0,0 +1,60 @@
+﻿using helloserve.com.AdventOfCode.Tests.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace helloserve.com.AdventOfCode.Tests
+{
+    public class Verses2016Day07Tests : TestClass
+    {
+        private Verses2016Day07 verses = new Verses2016Day07();
+
+        [Fact]
+        public void Part1_ParseIp()
+        {
+            string[] ipParts = verses.ParseIp("aaaa[bbbb]cccc");
+            Assert.True(ipParts.Length == 3);
+            Assert.True(ipParts[0] == "aaaa");
+            Assert.True(ipParts[1] == "bbbb");
+            Assert.True(ipParts[2] == "cccc");
+        }
+
+        [Fact]
+        public void Part1_ContainsAbba()
+        {
+            Assert.False(verses.ContainsAbba("aaaa"));
+            Assert.False(verses.ContainsAbba("ergt"));
+            Assert.True(verses.ContainsAbba("oxxo"));
+            Assert.False(verses.ContainsAbba("oxox"));
+            Assert.False(verses.ContainsAbba("oxo"));
+            Assert.True(verses.ContainsAbba("pooxxop"));
+        }
+
+        [Fact]
+        public void Part1_IsTls()
+        {
+            Assert.False(verses.IsTls("a[b]"));
+            Assert.False(verses.IsTls("aaaa[b]c"));
+            Assert.False(verses.IsTls("aaaa[bbbb]cccc"));
+            Assert.True(verses.IsTls("abba[mnop]qrst"));
+            Assert.False(verses.IsTls("abcd[bddb]xyyx"));
+            Assert.False(verses.IsTls("aaaa[qwer]tyui"));
+            Assert.True(verses.IsTls("nioxxoj[asdfgh]zxcvbn"));
+        }
+
+        [Fact]
+        public void Part1_Ex()
+        {
+            string input = "abba[mnop]qrst\r\nabcd[bddb]xyyx\r\naaaa[qwer]tyui\r\nioxxoj[asdfgh]zxcvbn";
+            Assert.True(verses.Part1(input) == 2);
+        }
+
+        [Fact]
+        public void Part1_Part1()
+        {
+            Assert.True(verses.Part1(ReadTextSource("7.txt")) == 2);
+        }
+    }
+}
