f00fe330faf8ca2fe1286154e7520222281167bd
helloserve <helloserve@gmail.com>
2016-12-08 07:52:40 +0200

    day 8 prep

---
+++ 2016/src/helloserve.com.AdventOfCode/Verses2016Day08.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace helloserve.com.AdventOfCode
+{
+    public class Verses2016Day08
+    {
+    }
+}
