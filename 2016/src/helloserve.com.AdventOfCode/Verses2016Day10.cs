830a76852d8e83ccb8df83e6983fc5c6b3511380
helloserve <helloserve@gmail.com>
2016-12-12 08:02:51 +0200

    day 10 part 1 WIP

---
+++ 2016/src/helloserve.com.AdventOfCode/Verses2016Day10.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace helloserve.com.AdventOfCode
+{
+    public class Verses2016Day10
+    {
+        public int Part1(string input, int value1, int value2)
+        {
+            string[] lines = input.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                ParseLine(input);
+            }            
+        }
+
+        private void ParseLine(string input)
+        {
+            string word = input.Substring(0, input.IndexOf(" "));
+            if (word == "value")
+                AssignValue(input);            
+        }
+
+        private void AssignValue(string input)
+        {
+
+        }
+    }
+}
---
+++ 2016/test/helloserve.com.AdventOfCode.Tests/Verses2016Day10Tests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace helloserve.com.AdventOfCode.Tests
+{
+    public class Verses2016Day10Tests
+    {
+        private Verses2016Day10 verses = new Verses2016Day10();
+
+        [Fact]
+        public void Part1_Simple()
+        {
+            Assert.True(verses.Part1("value 1 goes to bot 1\r\nvalue 2 goes to bot 1", 1, 2) == 1);
+        }
+    }
+}
