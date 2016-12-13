9b6c74352d7a9ca7d2d60bdfb409989bd2277b4c
Henk Roux <henk@globalkinetic.com>
2016-12-13 11:45:38 +0200

    prep day 11

---
+++ 2016/src/helloserve.com.AdventOfCode/Verses2016Day11.cs
@@ -0,0 +1,12 @@
+﻿using helloserve.com.AdventOfCode.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace helloserve.com.AdventOfCode
+{
+    public class Verses2016Day11 : Verses2016
+    {
+    }
+}
---
+++ 2016/test/helloserve.com.AdventOfCode.Tests/Verses2016Day11Tests.cs
@@ -0,0 +1,20 @@
+﻿using helloserve.com.AdventOfCode.Tests.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace helloserve.com.AdventOfCode.Tests
+{
+    public class Verses2016Day11Tests : TestClass
+    {
+        private Verses2016Day11 verses = new Verses2016Day11();
+
+        [Fact]
+        public void Part1_Simple()
+        {
+
+        }
+    }
+}
