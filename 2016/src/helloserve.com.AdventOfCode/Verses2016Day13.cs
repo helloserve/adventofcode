93b84448b213bca9e0dfb3487df8e2098bc79c89
Henk Roux <henk@globalkinetic.com>
2016-12-20 09:42:34 +0200

    day 13 part 1 WIP

---
+++ 2016/src/helloserve.com.AdventOfCode/Verses2016Day13.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace helloserve.com.AdventOfCode
+{
+    public class Verses2016Day13
+    {
+        public int BaseValue(int x, int y, int fav)
+        {
+            return x * x + 3 * x + 2 * x * y + y + y * y + fav;
+        }
+
+        public string Binary(int v)
+        {
+            byte[] buffer = BitConverter.GetBytes(v);
+            string bufferStr = string.Empty;
+            for (int i = buffer.Length - 1; i > 0; i--)
+            {
+                bufferStr = $"{bufferStr}{Convert.ToString(buffer[i], 2).PadLeft(8, '0')}";
+            }
+            bufferStr = $"{bufferStr}{Convert.ToString(buffer[0], 2).PadLeft(8, '0')}";
+
+            return bufferStr;
+        }
+
+        public int OnBits(string binary)
+        {
+            return binary.Replace("0", "").Length;
+        }
+
+        public bool IsOpen(int x, int y, int seed)
+        {
+            return OnBits(Binary(BaseValue(x, y, seed))) % 2 == 0;
+        }
+
+        public int DistanceBetween(int sx, int sy, int dx, int dy)
+        {
+            return (int)Math.Round(Math.Sqrt(Math.Pow(sx - dx, 2) + Math.Pow(sy - dy, 2)));
+        }
+
+        public int Route(Node startNode, int seed, int destX, int destY)
+        {
+            return 0;
+        }
+
+        public int Part1(int startX, int startY, int seed, int destX, int destY)
+        {
+            return Route(new Node()
+            {
+                X = startX,
+                Y = startY,
+                IsOpen = IsOpen(startX, startY, seed),
+                DistanceFrom = 0,
+                DistanceTo = DistanceBetween(startX, startY, destX, destY)
+            }, seed, destX, destY);
+        }
+    }
+
+    public class Node
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public bool IsOpen { get; set; }
+        public bool Visited { get; set; }
+        public int DistanceFrom { get; set; }
+        public int DistanceTo { get; set; }
+    }
+}
---
+++ 2016/test/helloserve.com.AdventOfCode.Tests/Verses2016Day13Tests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace helloserve.com.AdventOfCode.Tests
+{
+    public class Verses2016Day13Tests
+    {
+        [Fact]
+        public void Part1_BaseValue()
+        {
+            Verses2016Day13 verses = new Verses2016Day13();
+            Assert.True(verses.BaseValue(0, 0, 1) == 1);
+            Assert.True(verses.BaseValue(1, 1, 1) == 9);
+        }
+
+        [Fact]
+        public void Part1_Binary()
+        {
+            Verses2016Day13 verses = new Verses2016Day13();
+            Assert.True(verses.Binary(1) == "00000000000000000000000000000001");
+            Assert.True(verses.Binary(9) == "00000000000000000000000000001001");
+            Assert.True(verses.Binary(256) == "00000000000000000000000100000000");
+            Assert.True(verses.Binary(257) == "00000000000000000000000100000001");
+        }
+
+        [Fact]
+        public void Part1_OnBits()
+        {
+            Verses2016Day13 verses = new Verses2016Day13();
+            Assert.True(verses.OnBits("100") == 1);
+            Assert.True(verses.OnBits("101") == 2);
+            Assert.True(verses.OnBits("111") == 3);
+        }
+
+        [Fact]
+        public void Part1_IsWall()
+        {
+            Verses2016Day13 verses = new Verses2016Day13();
+            Assert.False(verses.IsOpen(0, 0, 1));
+            Assert.True(verses.IsOpen(0, 0, 3));
+            Assert.True(verses.IsOpen(0, 0, 257));
+            Assert.False(verses.IsOpen(0, 0, 259));
+        }
+
+        [Fact]
+        public void Part1_Ex()
+        {
+            Verses2016Day13 verses = new Verses2016Day13();
+            Assert.True(verses.Part1(1, 1, 10, 7, 4) == 11);
+        }
+    }
+}
