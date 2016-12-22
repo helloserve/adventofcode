3f782cea75e1d4e58ddf0ad00e58162bade36353
helloserve <helloserve@gmail.com>
2016-12-22 21:52:53 +0200

    include the actual code files.

---
+++ 2016/src/helloserve.com.AdventOfCode/Verses2016Day16.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace helloserve.com.AdventOfCode
+{
+    public class Verses2016Day16
+    {
+        public string Expand(string a, int length)
+        {
+            char[] result = a.ToCharArray();
+            while (result.Length < length)
+            {
+                char[] b = new char[result.Length];
+                for (int i = 0; i < result.Length; i++)
+                {
+                    b[result.Length - 1 - i] = result[i] == '1' ? '0' : '1';
+                }
+
+                char[] expandedResult = new char[result.Length + 1 + b.Length];
+                Array.Copy(result, expandedResult, result.Length);
+                expandedResult[result.Length] = '0';
+                Array.Copy(b, 0, expandedResult, result.Length + 1, b.Length);
+                result = expandedResult;
+            }
+
+            char[] finalResult = new char[length];
+            Array.Copy(result, finalResult, length);
+            return new string(finalResult);
+        }
+
+        public string Checksum(string value)
+        {
+            char[] input = value.ToCharArray();
+            char[] checksum = new char[0];
+            int checksumLength = 0;
+            while (checksumLength == 0 || checksumLength % 2 == 0)
+            {
+                checksum = new char[input.Length / 2];
+                int i = 0;
+                checksumLength = 0;
+                while (i + 1 < input.Length)
+                {
+                    if (input[i] == input[i + 1])
+                        checksum[checksumLength] = '1';
+                    else
+                        checksum[checksumLength] = '0';
+
+                    i += 2;
+                    checksumLength++;
+                }
+
+                input = checksum;
+            }
+
+            return new string(checksum);
+        }
+
+        public string Part1(string state, int size)
+        {
+            return Checksum(Expand(state, size));
+        }
+    }
+}
---
+++ 2016/test/helloserve.com.AdventOfCode.Tests/Verses2016Day16Tests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace helloserve.com.AdventOfCode.Tests
+{
+    public class Verses2016Day16Tests
+    {
+        [Fact]
+        public void Part1_Expand()
+        {
+            Verses2016Day16 verses = new Verses2016Day16();
+            Assert.True(verses.Expand("1", 3) == "100");
+            Assert.True(verses.Expand("0", 3) == "001");
+            Assert.True(verses.Expand("11111", 11) == "11111000000");
+            Assert.True(verses.Expand("111100001010", 25) == "1111000010100101011110000");
+        } 
+        
+        [Fact]
+        public void Part1_Checksum()
+        {
+            Verses2016Day16 verses = new Verses2016Day16();
+            Assert.True(verses.Checksum("1100") == "1");
+            Assert.True(verses.Checksum("1010") == "1");
+            Assert.True(verses.Checksum("110010110100") == "100");
+        }
+
+        [Fact]
+        public void Part1_Ex()
+        {
+            Verses2016Day16 verses = new Verses2016Day16();
+            Assert.True(verses.Part1("10000", 20) == "01100");
+        }
+
+        [Fact]
+        public void Part1_Part1()
+        { 
+            Verses2016Day16 verses = new Verses2016Day16();
+            Assert.True(verses.Part1("11100010111110100", 272) == "10100011010101011");
+        }
+
+        [Fact]
+        public void Part2_Part2()
+        {
+            Verses2016Day16 verses = new Verses2016Day16();
+            Assert.True(verses.Part1("11100010111110100", 35651584) == "01010001101011001");
+        }
+    }
+}
