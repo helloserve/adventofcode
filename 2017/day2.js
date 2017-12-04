2be0ce0411462abea88d8eac25af45fbf2e0b25f
helloserve <helloserve@gmail.com>
2017-12-04 21:05:53 +0200

    map / reduce 2017 day 2

--- 2017/day2.js
+++ 2017/day2.js
@@ -1,52 +1,58 @@
-part1 = (input) => {
-    var total = 0;
-    input.split('\r\n').forEach((line, index) => {            
-        var min = Number.MAX_SAFE_INTEGER;
-        var max = Number.MIN_SAFE_INTEGER;
-        line.split(/\s/).forEach((element, index) => { 
-            if (element * 1 < min) {
-                min = element * 1;
-            }
+part1 = (input) =>
+    input.split('\r\n').reduce((accumulator, currentLine, lineIndex, array) => {
+        if (currentLine.length == 0) {
+            return accumulator;
+        }
+        
+        var lineValues = { 
+            min : Number.MAX_SAFE_INTEGER, 
+            max : Number.MIN_SAFE_INTEGER 
+        };
 
-            if (element * 1 > max) {
-                max = element * 1;
+        lineValues = currentLine.split(/\s/).reduce((values, currentElement, elementIndex, elementArray) => {
+            var element = parseInt(currentElement);            
+            if (element < values.min) {
+                values.min = element;
+            }
+            if (element > values.max) {
+                values.max = element;
             }                
-        });
+            return values;
+        }, lineValues);
 
-        total += max - min;
-    });
+        return accumulator + lineValues.max - lineValues.min;
+    }, 0);
 
-    console.log(total);
-    return total;
-}
-
-part2 = (input) => {
-    var total = 0;
-    input.split('\r\n').forEach((line, index) => {            
-        var elements = line.split(/\s/);
-
-        for (let i = 0; i < elements.length; i++) {
-            const element_i = elements[i] * 1;
-            
-            for (let j = 0; j < elements.length; j++) {
-                if (i === j) {
-                    continue;
-                }
-
-                const element_j = elements[j] * 1;
-                
-                if (element_j > element_i || element_i % element_j !== 0) {
-                    continue;
-                }
-
-                total += element_i / element_j;
-            }
+part2 = (input) => 
+    input.split('\r\n').reduce((accumulator, currentLine, lineIndex, lineArray) => {
+        if (currentLine.length === 0) {
+            return accumulator;
         }
 
-    });
+        return accumulator + 
+            currentLine.split(/\s/).reduce((value, currentElement, elementIndex, elementArray) => {                
+                for (let i = 0; i < elementArray.length; i++) {
+                    const element_i = elementArray[i] * 1;
+                    
+                    for (let j = 0; j < elementArray.length; j++) {
+                        if (i === j) {
+                            continue;
+                        }
+        
+                        const element_j = elementArray[j] * 1;
+                        
+                        if (element_j > element_i || element_i % element_j !== 0) {
+                            continue;
+                        }
+        
+                        return element_i / element_j;
+                    }
+                }
 
-    console.log(total);
-    return total;        
-}
+                return 0;
+            }, 0);
+    }, 0);
 
 module.exports = { part1, part2 };
+
+console.log(part1('5 1 9 5\r\n7 5 3\r\n2 4 6 8\r\n'));
