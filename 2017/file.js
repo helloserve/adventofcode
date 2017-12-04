0cbdf01a1e16dfd4a818f856f5adb3a630174f18
helloserve <helloserve@gmail.com>
2017-12-04 19:28:18 +0200

    structure updates to better JS

--- 2017/file.js
+++ 2017/file.js
@@ -1,14 +1,10 @@
 var fs = require('fs');
 
-function File() {
-
-    this.load = function(filename, func) {
+load = function(filename, func) {
         fs.readFile(filename, 'utf8', (err, data) => {
             if (err) throw err;
             func(data);
         });
     }
 
-}
-
-module.exports = File;
+module.exports = { load };
--- 2017/tests/day3-tests.js
+++ 2017/tests/day3-tests.js
@@ -1,42 +1,35 @@
 var chai = require('chai');
 var expect = chai.expect;
-var Day3 = require('../day3.js');
-var path = require('path');
+var day3 = require('../day3.js');
 
 describe('Day 3', () => {
 
     it('Part 1 [1]', () => {
-        var day3 = new Day3();
         var result = day3.part1(1);
         expect(result).to.equal(0);
     });
 
     it('Part 1 [12]', () => {
-        var day3 = new Day3();
         var result = day3.part1(12);
         expect(result).to.equal(3);
     });
 
     it('Part 1 [23]', () => {
-        var day3 = new Day3();
         var result = day3.part1(23);
         expect(result).to.equal(2);
     });
 
     it('Part 1 [1024]', () => {
-        var day3 = new Day3();
         var result = day3.part1(1024);
         expect(result).to.equal(31);
     });
 
     it('Part 1', () => {
-        var day3 = new Day3();
         var result = day3.part1(325489);
         expect(result).to.equal(552);
     });
 
     it('Part 2', () => {
-        var day3 = new Day3();
         var result = day3.part2(325489);
         expect(result).to.equal(330785);
     });
