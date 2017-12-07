911f70142f7120b0a04b3abe2926e7a9c71c43f8
ARMORICA\henk <henk@globalkinetic.com>
2017-12-07 14:04:45 +0200

    2017 day 7 complete

--- 2017/tests/day7-tests.js
+++ 2017/tests/day7-tests.js
@@ -6,11 +6,6 @@ var path = require('path');
 
 describe('Day 7', () => {
 
-    // it('Part 1 [0 2 7 0]', () => {
-    //     var result = day7.part1('0 2 7 0');
-    //     expect(result).to.equal(5);
-    // });
-
     it('Part 1', function(done) { 
         file.load(path.resolve(__dirname, '../day7.txt'), (data) => {
             var result = day7.part1(data);
@@ -19,16 +14,11 @@ describe('Day 7', () => {
         });
     });
 
-    // it('Part 2 [2 4 1 2]', () => {
-    //     var result = day7.part2('2 4 1 2');
-    //     expect(result).to.equal(4);
-    // });
-
-    // it('Part 2', function(done) { 
-    //     file.load(path.resolve(__dirname, '../day7.txt'), (data) => {
-    //         var result = day7.part2(data);
-    //         console.log("DAY7 result", result);
-    //         expect(result).to.equals(2392).satisfy(done());
-    //     });
-    // });
+    it('Part 2', function(done) { 
+        file.load(path.resolve(__dirname, '../day7.txt'), (data) => {
+            var result = day7.part2(data);
+            console.log("DAY7 result", result);
+            expect(result).to.equals(1458).satisfy(done());
+        });
+    });
 });
