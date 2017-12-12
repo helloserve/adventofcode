b3b22414f5bd30ad0ade25330d78b2dc316b1807
helloserve <helloserve@gmail.com>
2017-12-12 18:24:32 +0200

    2017 day 12 complete

--- 2017/day12.js
+++ 2017/day12.js
@@ -7,51 +7,61 @@ const mapNodeLinks = (item) => item.match(/\d+/g).slice(1).reduce((accumulator, 
 
 const buildGraph = (input) => input.reduce((accumulator, item, index, array) => 
     [...accumulator, { 
-        toZero: index === 0, 
+        toTarget: false,
+        visited: false,        
         links: mapNodeLinks(item) 
     }], []);
 
-const linkToZero = (index, links, graph) => {
-    for (let i = 0; i < links.length; i++) {
-        graph[links[i]].toZero = true;        
+const linkToTarget = (index, graph, visitList) => {
+    var node = graph[index];
+    if (node.visited) {
+        return;
     }
-}
-
-const routeToZero = (graph) => {
-    var change = true;
-    while (change) {
-        change = false;
-        for (let i = 0; i < graph.length; i++) {
-            const item = graph[i];
-            for (let j = 0; j < item.links.length; j++) {
-                const element = item.links[j];
-                if (graph[element].toZero) {
-                    item.toZero = true;
-                    change = true;
-                }
-            }
-            if (item.toZero) {
-                linkToZero(i, item.links, graph);
-            }
+    node.visited = true;
+    for (let j = 0; j < node.links.length; j++) {        
+        const link = node.links[j];
+        if (link === index) {
+            continue;
         }
+        if (graph[link].toTarget) {
+            node.toTarget = true;
+            continue;
+        }
+        if (node.toTarget) {
+            graph[link].toTarget = true;
+        }
+        linkToTarget(link, graph);
     }
-    return graph.reduce((accumulator, item, index, array) => accumulator + (item.toZero ? 1 : 0), 0);
 }
 
+const routeToPipe = (graph, pipe) => {
+    graph[pipe].toTarget = true;
+    linkToTarget(pipe, graph);
+}
+    
 const part1 = (input) => {
     var graph = buildGraph(splitOnNewLine(input));
-    return routeToZero(graph);
+    routeToPipe(graph, 0);
+    return graph.reduce((accumulator, item, index, array) => accumulator + (item.toTarget ? 1 : 0), 0);    
 }
 
 const part2 = (input) => {
+    var graph = buildGraph(splitOnNewLine(input));
+    var count = 0;
+    var pipe = 0;
+    while (pipe !== -1) {
+        count++;
+        routeToPipe(graph, pipe);
+        pipe = graph.findIndex((node) => !node.toTarget);
+    }
+    return count;
 }
     
 module.exports =  { part1, part2 }
 
-//var input = '0 <-> 2\r\n1 <-> 1\r\n2 <-> 0, 3, 4\r\n3 <-> 2, 4\r\n4 <-> 2, 3, 6\r\n5 <-> 6\r\n6 <-> 4, 5';
-//console.log(part1(input));
+//console.log(part2('0 <-> 2\r\n1 <-> 1\r\n2 <-> 0, 3, 4\r\n3 <-> 2, 4\r\n4 <-> 2, 3, 6\r\n5 <-> 6\r\n6 <-> 4, 5'));
 
-file.load(path.resolve(__dirname, './day12.txt'), (data) => {
-    var result = part1(data);
-    console.log("day12 result", result);
-});
+// file.load(path.resolve(__dirname, './day12.txt'), (data) => {
+//     var result = part2(data);
+//     console.log(result);
+// });
