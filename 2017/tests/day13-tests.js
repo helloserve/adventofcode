9fe01de9274ad83518d824651874fd8bed27246c
helloserve <helloserve@gmail.com>
2017-12-14 07:09:32 +0200

    2017 day 13 rekt

--- 2017/tests/day13-tests.js
+++ 2017/tests/day13-tests.js
@@ -18,10 +18,10 @@ describe('Day 13', () => {
         });
     });
 
-    // it('Part 2 ex', () => {
-    //     var result = day13.part2('0 <-> 2\r\n1 <-> 1\r\n2 <-> 0, 3, 4\r\n3 <-> 2, 4\r\n4 <-> 2, 3, 6\r\n5 <-> 6\r\n6 <-> 4, 5');
-    //     expect(result).to.equal(2);
-    // });
+    it('Part 2 ex', () => {
+        var result = day13.part2('0: 3\r\n1: 2\r\n4: 4\r\n6: 4');
+        expect(result).to.equal(10);
+    });
 
     // it('Part 2', function(done) { 
     //     file.load(path.resolve(__dirname, '../day13.txt'), (data) => {
